b03bb70b8d563be565e86efd4a9740abe5fa41e6
Jonathan Soukaseume <sksmejn@gmail.com>
2021-05-07 16:15:27 -0700

    made the database connect to atlas only

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 
-const dbserver = process.env.MONGOD_URI || "mongodb://localhost/lifedash_db";
-
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(dbserver, {
+    const connect = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
@@ -12,7 +10,7 @@ const connectDB = async () => {
     });
     console.log(`DB Connected: ${connect.connection.host}`);
   } catch (err) {
-    console.err(err);
+    console.log(err);
     process.exit(1);
   }
 };
