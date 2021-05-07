5c4218c45b5ad08a4d632a74d7136c10c3795544
Jonathan Soukaseume <sksmejn@gmail.com>
2021-05-07 11:07:24 -0700

    added profile model and adjusted scripts in package.json to reflect new modules installed: nodemon, cross-env, express-session, fs-extra. Getting ready for auth installation

--- server/controllers/profile/index.js
+++ server/controllers/profile/index.js
@@ -1,10 +1,23 @@
 const router = require("express").Router();
+const { Profile } = require("../../models");
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   try {
+    const profile = await Profile.find({});
+    console.log(profile);
     res.status(200).send("Profile Routes Here");
   } catch (err) {
     console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post("/create", async (req, res) => {
+  try {
+    console.log("POST: Create Profile");
+    res.send("Create profile");
+  } catch (err) {
+    console.log(err.message);
     res.status(500).json(err);
   }
 });
