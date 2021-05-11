7901cf450ffc7f4235ae630a78f78a583f685cce
Jonathan Soukaseume <sksmejn@gmail.com>
2021-05-11 15:40:01 -0700

    removed bar from profile to prevent bar from appearing twice

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
-import Bar from '../components/Bar';
-import ProfileHeader from '../components/ProfileHeader';
-import SwiperPics from '../components/Swiper/Swiper';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors";
+import Bar from "../components/Bar";
+import ProfileHeader from "../components/ProfileHeader";
+import SwiperPics from "../components/Swiper/Swiper";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,17 +11,17 @@ const useStyles = makeStyles((theme) => ({
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: "56.25%", // 16:9
   },
   expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
       duration: theme.transitions.duration.shortest,
     }),
   },
   expandOpen: {
-    transform: 'rotate(180deg)',
+    transform: "rotate(180deg)",
   },
   avatar: {
     backgroundColor: red[500],
@@ -37,10 +37,9 @@ export default function RecipeReviewCard() {
   };
 
   return (
-    <>  
-    <Bar />
-    <ProfileHeader />
-    <SwiperPics />
+    <>
+      <ProfileHeader />
+      <SwiperPics />
     </>
   );
 }
