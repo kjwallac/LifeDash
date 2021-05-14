8d230c67a007944321a94f863b894afa8fbbb51c
Jonathan Soukaseume <sksmejn@gmail.com>
2021-05-13 18:37:20 -0700

    added card styles to options and background color

--- client/src/pages/Account/Account.js
+++ client/src/pages/Account/Account.js
@@ -5,19 +5,35 @@ import { API } from "../../utils/API";
 export const Account = (props) => {
   // States
   const [name, setName] = useState(null);
+  const [image, setImage] = useState(null);
+  const [userID, setUserID] = useState(null);
 
   useEffect(() => {
     const getUser = async () => {
       const id = props.match.params.id;
       const user = await API.getUser(id);
       setName(user.data.displayName);
+      setImage(user.data.image);
+      setUserID(id);
     };
     return getUser();
   });
 
   return (
-    <div>
-      <h1>Welcome {name}</h1>
+    <div className="account-container">
+      <h1>Welcome!</h1>
+      <div className="image-container">
+        <div className="name-icon">
+          <img src={image} alt="profile pic" />
+          <p>{name}</p>
+        </div>
+      </div>
+      <div className="acc-options-container">
+        <h2>Account</h2>
+        <div className="acc-options">View Profiles</div>
+        <div className="acc-options">Create new profile</div>
+        <div className="acc-options">Logout</div>
+      </div>
     </div>
   );
 };
