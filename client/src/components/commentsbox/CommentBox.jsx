31a138b8dbbc9c464ceeaf5118a288b0be408cb6
Ernie Perez <38770396+jep1220@users.noreply.github.com>
2021-05-17 20:23:49 -0700

    npm i classnames

--- client/src/components/commentsbox/CommentBox.jsx
+++ client/src/components/commentsbox/CommentBox.jsx
@@ -1,90 +1,85 @@
-import React, {useState, useRef} from "react";
-//import cn from "classnames";
-import "../styles.css";
+import React, { useState, useRef } from "react";
+import cn from "classnames";
+import useDynamicHeightField from "./useDynamicHeightField";
+import "./styles.css";
 
-const INITIAL_HEIGHT =46;
-
-const CommentBox = () => {
-    const [isExpanded, setIsExpanded] = useState(false);
-    const [commnetValue, setCommentValue] = useState("")
+const INITIAL_HEIGHT = 46;
 
 
-const outerHeight = useRef(INITIAL_HEIGHT);
-const textRef = useRef(null);
-const containerRef = useRef(null);
+export default function CommentBox() {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [commentValue, setCommentValue] = useState("");
 
-const onExpand = () => {
+  const outerHeight = useRef(INITIAL_HEIGHT);
+  const textRef = useRef(null);
+  const containerRef = useRef(null);
+  useDynamicHeightField(textRef, commentValue);
+
+  const onExpand = () => {
     if (!isExpanded) {
-        outerHeight.current = containerRef.current.scrollHeight;
-        setIsExpanded(true);
-    } 
-        
-};
+      outerHeight.current = containerRef.current.scrollHeight;
+      setIsExpanded(true);
+    }
+  };
 
-const onChange = (e) => {
+  const onChange = (e) => {
     setCommentValue(e.target.value);
-};
+  };
 
-const onClose = () => {
+  const onClose = () => {
     setCommentValue("");
     setIsExpanded(false);
-};
+  };
 
-const onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    console.log('send the form data somewhere')
-}
+    console.log("send the form data somewhere");
+  };
 
-return (
-    <form
-    onSubmit={onSubmit}
-    ref={containerRef}
-    className={("comment-box", {
-        expanded: isExpanded,
-        collapsed: !isExpanded,
-        modified: commnetValue.length > 0, 
-    })}
-    style={{
-        minHeight: isExpanded ? outerHeight.current: INITIAL_HEIGHT
-    }}
-    >
-
-    <div className="header">
-        <div className="user">
+  return (
+    <div className="container">
+      <form
+        onSubmit={onSubmit}
+        ref={containerRef}
+        className={cn("comment-box", {
+          expanded: isExpanded,
+          collapsed: !isExpanded,
+          modified: commentValue.length > 0
+        })}
+        style={{
+          minHeight: isExpanded ? outerHeight.current : INITIAL_HEIGHT
+        }}
+      >
+        <div className="header">
+          <div className="user">
             <img
-            src="avatar/path"
-            alt="use avatar"
+              src=""
+              alt="User avatar"
             />
             <span>User Name</span>
-            </div>
-            </div>
-
-
-    <label htmlFor="comment">Write a Comment</label>
-    <textarea
-        ref={textRef}
-        onClick={onExpand}
-        onFocus={onExpand}
-        onChange={onChange}
-        className="comment-field"
-        placeholder="Write a Comment"
-        value={commnetValue}
-        name="comment"
-        id="comment"
+          </div>
+        </div>
+        <label htmlFor="comment">What are your thoughts?</label>
+        <textarea
+          ref={textRef}
+          onClick={onExpand}
+          onFocus={onExpand}
+          onChange={onChange}
+          className="comment-field"
+          placeholder="What are your thoughts?"
+          value={commentValue}
+          name="comment"
+          id="comment"
         />
-
-    <div className="actions">
-        <button type="button" className="cancel" onClick={onClose}>
-            cancel
-        </button>
-        <button type="submit" disabled={commnetValue.length <1}>
-            Add Comment
-        </button>
-        </div>  
-    </form>
-   
-);
-
-};
-
-export default CommentBox;
+        <div className="actions">
+          <button type="button" className="cancel" onClick={onClose}>
+            Cancel
+          </button>
+          <button type="submit" disabled={commentValue.length < 1}>
+            Respond
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
