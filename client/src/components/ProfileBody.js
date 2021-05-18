2fdeba4279d29c88ee87adc6ca3ab45c20707bde
Ernie Perez <38770396+jep1220@users.noreply.github.com>
2021-05-17 19:54:47 -0700

    saved profilebodyjs

--- client/src/components/ProfileBody.js
+++ client/src/components/ProfileBody.js
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import CommentBox from './commentsbox/CommentBox';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
