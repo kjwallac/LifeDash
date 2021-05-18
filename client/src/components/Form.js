100beaefcde0ba14e4e4338113af77e7a1fe6635
kjwallac <kristinajwallace@gmail.com>
2021-05-17 21:15:22 -0700

    attempting to add profile form upload

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import { makeStyles } from "@material-ui/core/styles";
-import Input from "@material-ui/core/Input";
-import InputLabel from "@material-ui/core/InputLabel";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import FormControl from "@material-ui/core/FormControl";
+// import Input from "@material-ui/core/Input";
+// import InputLabel from "@material-ui/core/InputLabel";
+// import InputAdornment from "@material-ui/core/InputAdornment";
+// import FormControl from "@material-ui/core/FormControl";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import AccountCircle from "@material-ui/icons/AccountCircle";
-import AddPhotoAlternateRoundedIcon from "@material-ui/icons/AddPhotoAlternateRounded";
+// import AddPhotoAlternateRoundedIcon from "@material-ui/icons/AddPhotoAlternateRounded";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import SaveIcon from "@material-ui/icons/Save";
+import { executeApiRequest } from "../utils/apiHelper";
+import Container from "@material-ui/core/Container";
+// import CssBaseline from "@material-ui/core/CssBaseline";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,152 +34,203 @@ const useStyles = makeStyles((theme) => ({
 export default function Form() {
   const classes = useStyles();
 
-  const [fields, setFields] = useState([{ value: null }]);
+  const [profile, setProfile] = useState({
+    firstName: "",
+    lastName: "",
+    bornDate: "",
+    deathDate: "",
+    quote: "",
+    bio: "",
+  });
 
-  function handleChange(i, event) {
-    const values = [...fields];
-    values[i].value = event.target.value;
-    setFields(values);
+  function genOnFieldChange(fieldName) {
+    debugger;
+    return (event) => {
+      const newProfile = { ...profile, [fieldName]: event.target.value };
+      setProfile(newProfile);
+    };
   }
 
-  function handleAdd() {
-    const values = [...fields];
-    values.push({ value: null });
-    setFields(values);
+  async function submitProfile() {
+    executeApiRequest("api/profile/create", "POST", profile);
   }
+
+  // start adds additional input fields for photo links
+  // function handleChange(i, event) {
+  //   const values = [...fields];
+  //   values[i].value = event.target.value;
+  //   setFields(values);
+  // }
+  // function handleAdd() {
+  //   const values = [...fields];
+  //   values.push({ value: null });
+  //   setFields(values);
+  // }
+  // end adds additional input fields for photo links
 
   return (
     <>
-      <div className={classes.margin}>
-        <Grid container spacing={1} alignItems="flex-end" fullWidth>
-          <Grid item>
-            <AccountCircle />
-          </Grid>
-          <Grid item>
-            <TextField
-              inputProps={{
-                maxLength: 50,
-              }}
-              id="firstName"
-              label="First Name"
-              input
-              type="text"
-              required="true"
-              fullWidth
-            />
-          </Grid>
-        </Grid>
-      </div>
-      <div className={classes.margin}>
-        <Grid container spacing={1} alignItems="flex-end">
-          <Grid item>
-            <AccountCircle />
-          </Grid>
-          <Grid item>
-            <TextField
-              inputProps={{
-                maxLength: 50,
-              }}
-              id="lastName"
-              label="Last Name"
-              input
-              type="text"
-            />
-          </Grid>
-        </Grid>
-      </div>
-      <div className={classes.margin}>
-        <Grid container spacing={1} alignItems="flex-end">
-          <Grid item>
-            <AccountCircle />
-          </Grid>
-          <Grid item>
-            <TextField
-              inputProps={{
-                maxLength: 4,
-              }}
-              id="bornDate"
-              label="Year of Birth"
-              type="number"
-            />
-          </Grid>
-        </Grid>
-      </div>
-      <div className={classes.margin}>
-        <Grid container spacing={1} alignItems="flex-end">
-          <Grid item>
-            <AccountCircle />
-          </Grid>
-          <Grid item>
-            <TextField id="bornDate" label="Year of Passing" />
-          </Grid>
-        </Grid>
-      </div>
-      <div className={classes.margin}>
-        <Grid container spacing={1} alignItems="flex-end">
-          <Grid item>
-            <AccountCircle />
-          </Grid>
-          <Grid item>
-            <TextField id="input-with-icon-grid" label="Year of Birth" />
-          </Grid>
-        </Grid>
-      </div>
-
-      <div className={classes.margin}>
-        <Grid container spacing={1} alignItems="flex-end">
-          <Grid item>
-            <AccountCircle />
-          </Grid>
-          <Grid item>
-            <TextField id="input-with-icon-grid" label="Quote" />
-          </Grid>
-        </Grid>
-      </div>
-
-      <div className={classes.margin}>
-        <Grid container spacing={1} alignItems="flex-end">
-          <Grid item>
-            <AccountCircle />
-          </Grid>
-          <Grid item>
-            <TextField
-              id="input-with-icon-grid"
-              label="Biography"
-              fullWidth
-              multiline
-            >
-              <input />
-            </TextField>
-          </Grid>
-        </Grid>
-      </div>
-      {/* begin code to duplicate each time new photo is click */}
-      {fields.map((field, idx) => {
-        return (
-          <div key={`${field}-${idx}`} className={classes.margin}>
-            <Grid container spacing={1} alignItems="flex-end">
-              <Grid item>
-                <AccountCircle />
-              </Grid>
-              <Grid>
-                <TextField
-                  id="input-with-icon-grid"
-                  label="Photo links"
-                  onChange={(e) => handleChange(idx, e)}
-                ></TextField>
-              </Grid>
-              <Grid item>
-                <AddPhotoAlternateRoundedIcon
-                  type="button"
-                  onClick={() => handleAdd()}
-                />
-              </Grid>
+      <Container maxWidth="sm">
+        <div className={classes.margin}>
+          <Grid container spacing={1} alignItems="flex-end" fullWidth>
+            <Grid item>
+              <AccountCircle />
             </Grid>
-          </div>
-        );
-      })}
-      {/* end code to duplicate each time new photo is click */}
+            <Grid item>
+              <TextField
+                inputProps={{
+                  maxLength: 50,
+                }}
+                id="firstName"
+                label="First Name"
+                input
+                type="text"
+                required="true"
+                value={profile.firstName}
+                onChange={genOnFieldChange("firstName")}
+              />
+            </Grid>
+          </Grid>
+        </div>
+        <div className={classes.margin}>
+          <Grid container spacing={1} alignItems="flex-end">
+            <Grid item>
+              <AccountCircle />
+            </Grid>
+            <Grid item>
+              <TextField
+                inputProps={{
+                  maxLength: 50,
+                }}
+                id="lastName"
+                label="Last Name"
+                input
+                type="text"
+                required="true"
+                value={profile.lastName}
+                onChange={genOnFieldChange("lastName")}
+              />
+            </Grid>
+          </Grid>
+        </div>
+        <div className={classes.margin}>
+          <Grid container spacing={1} alignItems="flex-end">
+            <Grid item>
+              <AccountCircle />
+            </Grid>
+            <Grid item>
+              <TextField
+                inputProps={{
+                  maxLength: 4,
+                }}
+                id="bornDate"
+                label="Year of Birth"
+                type="number"
+                required="true"
+                value={profile.bornDate}
+                onChange={genOnFieldChange("bornDate")}
+
+              />
+            </Grid>
+          </Grid>
+        </div>
+        <div className={classes.margin}>
+          <Grid container spacing={1} alignItems="flex-end">
+            <Grid item>
+              <AccountCircle />
+            </Grid>
+            <Grid item>
+              <TextField
+                id="deathDate"
+                label="Year of Passing"
+                required="true"
+                value={profile.deathDate}
+                onChange={genOnFieldChange("deathDate")}
+              />
+            </Grid>
+          </Grid>
+        </div>
+        <div className={classes.margin}>
+          <Grid container spacing={1} alignItems="flex-end">
+            <Grid item>
+              <AccountCircle />
+            </Grid>
+            <Grid item>
+              <TextField
+                id="quote"
+                label="Quote"
+                required="true"
+                value={profile.quote}
+                onChange={genOnFieldChange("quote")}
+              />
+            </Grid>
+          </Grid>
+        </div>
+
+        <div className={classes.margin}>
+          <Grid container spacing={1} alignItems="flex-end">
+            <Grid item>
+              <AccountCircle />
+            </Grid>
+            <Grid item>
+              <TextField
+                id="bio"
+                label="Biography"
+                required="true"
+                value={profile.bio}
+                onChange={genOnFieldChange("bio")}
+
+                multiline
+              >
+                <input />
+              </TextField>
+            </Grid>
+          </Grid>
+        </div>
+        {/* begin code to duplicate each time new photo is click */}
+        {/* {fields.map((field, idx) => {
+          return (
+            <div key={`${field}-${idx}`} className={classes.margin}>
+              <Grid container spacing={1} alignItems="flex-end">
+                <Grid item>
+                  <AccountCircle />
+                </Grid>
+                <Grid>
+                  <TextField
+                    id="input-with-icon-grid"
+                    label="Photo links"
+                    onChange={(e) => handleChange(idx, e)}
+                  ></TextField>
+                </Grid>
+                <Grid item>
+                  <AddPhotoAlternateRoundedIcon
+                    type="button"
+                    onClick={() => handleAdd()}
+                  />
+                </Grid>
+              </Grid>
+            </div>
+          );
+        })} */}
+        {/* end code to duplicate each time new photo is click */}
+        <Grid className={classes.buttonRoot} item xs={3}>
+          <ButtonGroup
+            color="primary"
+            aria-label="outlined primary button group"
+          >
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              className={classes.button}
+              startIcon={<SaveIcon />}
+              onClick={submitProfile}
+            >
+              Save
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Container>
     </>
   );
 }
--- client/src/pages/Create/CreateProfile.js
+++ client/src/pages/Create/CreateProfile.js
@@ -1,5 +1,6 @@
 import "./CreateProfile.css";
 import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+import Form from "../../components/Form";
 
 export const CreateProfile = () => {
   return (
@@ -14,31 +15,8 @@ export const CreateProfile = () => {
         }}
       />
       <button>Add profile image!</button>
-      <div className="card-container">
-        <div className="card">
-          <div className="card-body">
-            <div className="form-group">
-              <label htmlFor="firstName">First Name</label>
-              <input type="text" maxLength="25" id="firstName" />
-              <label htmlFor="lastName">Last Name</label>
-              <input type="text" maxLength="30" />
-              <label htmlFor="bornDate">Date of birth</label>
-              <input type="number" min="0500" max="9999" />
-              <label htmlFor="deathDate">Date of passing</label>
-              <input type="number" min="0500" max="9999" />
-              <label htmlFor="profileImage">Profile Image</label>
-              <input type="text" />
-              <label htmlFor="bio">Bio</label>
-              <textarea name="bio" id="bio"></textarea>
-              <label htmlFor="status">Set Status</label>
-              <select name="status" id="status">
-                <option value="public">Public</option>
-                <option value="Private">Private</option>
-              </select>
-            </div>
-          </div>
-        </div>
-      </div>
+
+      <Form />
     </div>
   );
 };
